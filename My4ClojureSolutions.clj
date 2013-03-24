79c19d3f040f8ad27d89ce34bdadf08b80ba37a7
Gurpreet Saini <geezsaini@yahoo.com>
2013-03-24 12:12:42 -0700

    piddily diddily

--- My4ClojureSolutions.clj
+++ My4ClojureSolutions.clj
@@ -10,7 +10,7 @@
 ; my-*-classic: Solution based on classic top down recurson (e.g. loop/recur/trampoline)
 ; my-*: My attempt at relatively more idiomadic Clojure code (e.g. seq lib plus map/filter and/or seq lib plus for)
 
-; nth
+; nth ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;(defn my-nth (sq)
 ;  (nth sq))
 
@@ -24,7 +24,7 @@
 (defn my-nth [sq n]
   (last (take (inc n) sq)))
 
-;count
+;count ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; (defn my-nth (sq)
 ;   (count sq))
 
@@ -51,3 +51,73 @@
 ;    (for [i (iterate inc 0)
 ;          ele sq]
 ;      (list i ele))))) 
+
+; nil map key  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;the tests
+(true?  (map-entry-nil? :a {:a nil :b 2}))
+(false? (map-entry-nil? :b {:a nil :b 2}))
+(false? (map-entry-nil? :c {:a nil :b 2}))
+
+;the sol
+(defn map-entry-nil? [ky mp]
+  (let [ent (get mp ky :empty)]
+    (and (not (= ent :empty))
+         (nil? ent))))
+
+; a half truth ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; at least one are true = true
+; all are true = false
+; all are false = false
+
+
+;tests
+(= false (half-truth? false false)
+(= true (half-truth? true false))
+(= true (__ false true false))
+
+;sol
+(defn half-truth? [& args]
+  (and
+   (not (nil? (some true? args)))
+   (not-every? true? args)))
+
+; learning about -> ;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; test
+(defn threading-expr []
+  (= (last (sort (rest (reverse [2 5 4 1 3 6]))))
+     (-> [2 5 4 1 3 6] reverse rest sort last)
+     5))
+
+(threading-expr)
+
+;note "last" is the answer
+
+; summing ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; tests
+(= (my-sum2 [1 2 3]) 6)
+
+(= (__ (list 0 -2 5 5)) 8)
+
+(= (__ #{4 2 1}) 7)
+
+(= (__ '(0 0 -1)) -1)
+ 
+(= (__ '(1 10 3)) 14)
+
+
+;sol
+(defn my-sum [sq]
+  (apply + sq))
+
+; destructure bucture
+(defn my-sum2 [& args]
+  (+ args))
+
+
+
+  
+        
