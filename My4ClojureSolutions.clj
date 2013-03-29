5e9e42d6bca3fcce0f9b73c49f5967a1da3bffb6
Gurpreet Saini <geezsaini@yahoo.com>
2013-03-28 20:02:38 -0700

    cutting some teeth on fibbonaci
    so hard

--- My4ClojureSolutions.clj
+++ My4ClojureSolutions.clj
@@ -83,18 +83,6 @@
    (not (nil? (some true? args)))
    (not-every? true? args)))
 
-; learning about -> ;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; test
-(defn threading-expr []
-  (= (last (sort (rest (reverse [2 5 4 1 3 6]))))
-     (-> [2 5 4 1 3 6] reverse rest sort last)
-     5))
-
-(threading-expr)
-
-;note "last" is the answer
-
 ; summing ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; tests
@@ -117,7 +105,110 @@
 (defn my-sum2 [& args]
   (+ args))
 
+; recurring theme ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(loop [x 5
+       result []]
+  (if (> x 0)
+    (recur (dec x) (conj result (+ 2 x)))
+    result))
+; x  result
+; 5 (conj [] 7 )
+; 4 (conj [7] 6 )      
+; 3 (conj [7 6] 5)
+; 2 (conj [7 6 5] 4)
+; 1 (conj [7 6 5 4] 3)
+; 0 [7 6 5 4 3]
 
+; only odd numbers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn get-odd [sq]
+  (filter #(odd? %) sq)) 
+
+; learning about -> ;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; test
+(defn threading-expr []
+  (= (last (sort (rest (reverse [2 5 4 1 3 6]))))
+     (-> [2 5 4 1 3 6] reverse rest sort last)
+     5))
+
+(threading-expr)
+
+;note "last" is the answer
+
+; rearranging code ->> ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(= (apply + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+   (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (apply +))
+   11)
+
+; note "apply +" is the answer 
+
+; reversing a sequence ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; restrictions: rseq and reverse
+
+(defn my-reverse-classic [sq]
+  (loop [stepper sq
+         result []]
+    (if (empty? stepper) result
+        (recur (rest stepper)
+               (cons (first stepper)
+                     result)))))
+
+(defn my-reverse [sq]
+  (list
+   (butlast sq)
+   (last sq)))
+
+; fibbonaci numbers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; basis: F(0) = 0, F(1) = 1
+; induction: For F(N), N > 1, F(N) = F(N-1) + F(N-2)
+; (0 1 1 2 3 5 8 13 21 34)
+
+(defn my-fibo-classic 
+  "getting the nth, this totally blows the stack for large N dude, O(N) stack frames, are you kidding me?"
+  [n]
+  (cond (= n 0) 0
+        (= n 1) 1
+        :else
+        (+ (my-fibo-classic (- n 1))
+           (my-fibo-classic (- n 2)))))
+
+(defn my-fibo-tail
+  "getting the nth, using recur instead of self recursion "
+  [n]
+  (loop [fn1 0
+         fn2 1
+         i n]
+    (if (zero? i)
+      fn1
+      (recur fn2 (+ fn1 fn2) (dec i)))))
+
+;starting at i = 2
+(defn my-fibo-tail-acc1
+  "getting the stream of 0..n, using loop/recur instead of self recursion "
+  [n]
+  (loop [i 2
+         acc '[0 1]]
+      (if (>= i n)
+        acc
+        (let [fibbonaci-number (+ (nth acc (- i 1)) 
+                                  (nth acc (- i 2)))]
+          (recur (inc i)
+                 (conj acc fibbonaci-number))))))
+
+ 
+(defn my-fibo-tail-acc2*
+  "getting the stream of 0..n, using fn call/recur instead of self recursion "
+  [fn1 fn2 i acc]
+    (if (zero? i)
+      (cons 0 acc)
+      (recur fn2 
+             (+ fn1 fn2) 
+             (dec i)
+             (conj acc (+ fn1 fn2)))))
+
+(defn my-fibo-tail-acc2 [n]
+  (my-fibo-tail-acc2* 0 1 n '[]))
   
-        
+
